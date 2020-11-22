970b07afae36400cc9254f1414a66da31a68ddc9
Diego Alberto <dmalberto@outlook.com>
2020-11-22 19:14:03 -0300

    Add: MongoDB connection

--- main.py
+++ main.py
@@ -1,33 +1,23 @@
-from typing import Optional
 from planet import Planet
 from fastapi import FastAPI
-from pymongo import MongoClient
-import json
 
 app = FastAPI()
-
-client = MongoClient('127.0.0.1', 27017)
-db = client['star-wars-py']
-planets = db['planets']
 
 
 @app.post("/planets")
 def add_planet(planet: Planet):
-    planet.get_films_no()
-    return planet
+    planet.id = planet.get_id()
+    planet.films_no = planet.get_films_no()
+    planet.insert()
+    return {"status": 200, "response": planet}
 
 
 @app.delete("/planets/{id}")
-def delete_planet(id: str, q: Optional[str] = None):
-    return {"status": 200}
+def delete_planet(id: str):
+    return Planet.delete(id)
 
 
 @app.get("/planets")
 async def get_planet(id: int = None, name: str = None):
-    if name is not None:
-        response = planets.find_one({"name": name})
-    elif id is not None:
-        response = planets.find_one({"_id": id})
-    else:
-        response = planets.find_one()
-    return json.loads(json.dumps(response))
+    response = Planet.get(id, name)
+    return {"status": 200, "response": response}
--- planet.py
+++ planet.py
@@ -1,13 +1,59 @@
 from pydantic import BaseModel
 from typing import Optional
+import requests
+import json
+from pymongo import MongoClient
+
+with open('./config/credentials.json') as json_file:
+    credentials = json.load(json_file)
+client = MongoClient(
+    f"mongodb://{credentials['mongodb']['user']}:{credentials['mongodb']['pass']}@star-wars-shard-00-00.eljo1.mongodb.net:27017,star-wars-shard-00-01.eljo1.mongodb.net:27017,star-wars-shard-00-02.eljo1.mongodb.net:27017/{credentials['mongodb']['db']}?ssl=true&replicaSet=atlas-tso1zz-shard-0&authSource=admin&retryWrites=true&w=majority"
+)
+db = client['star-wars-py']
+planets = db['planets']
 
 
 class Planet(BaseModel):
-    _id: Optional[int]
+    id: Optional[int]
     name: str
     climate: str
     terrain: str
-    films_no: Optional[str] = None
+    films_no: Optional[int]
 
     def get_films_no(self):
-        self.films_no = "Creu"
+        results = json.loads(
+            requests.get(f"https://swapi.dev/api/planets/?search={self.name}").
+            content)['results']
+        if len(results) > 0:
+            return len(results[0]['films'])
+        else:
+            return 0
+
+    def insert(self):
+        planet = {
+            '_id': self.id,
+            'name': self.name,
+            'climate': self.climate,
+            'terrain': self.terrain,
+            'films_no': self.films_no
+        }
+        planets.insert_one(planet)
+
+    @staticmethod
+    def get(id, name):
+        if id is not None:
+            p = {"_id": id}
+        elif name is not None:
+            p = {"name": name}
+        else:
+            p = {}
+        return [r for r in planets.find(p)]
+
+    @staticmethod
+    def get_id():
+        return len([r for r in planets.find({})]) + 1
+
+    @staticmethod
+    def delete(id):
+        planets.delete_one({"_id": id})
+        return {"status": 200}
