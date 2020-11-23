3b2bc53ffb18cf21617c5909bcc540203a881039
Diego Alberto <dmalberto@outlook.com>
2020-11-22 21:27:52 -0300

    Refactor: isolate MongoDB

---
+++ mongo.py
@@ -0,0 +1,29 @@
+from fastapi import params
+from pymongo import MongoClient
+import json
+
+
+class Mongo():
+    def __init__(self):
+        self.credentials = self.__get_credentials()
+        self.client = self.__connect()
+        self.db = self.__get_database()
+
+    def __get_credentials(self):
+        with open('./config/credentials.json') as json_file:
+            credentials = json.load(json_file)
+        return credentials['mongodb']
+
+    def __connect(self):
+        login = f"{self.credentials['user']}:{self.credentials['pass']}"
+        url = "star-wars-shard-00-00.eljo1.mongodb.net:27017,star-wars-shard-00-01.eljo1.mongodb.net:27017,star-wars-shard-00-02.eljo1.mongodb.net:27017"
+        params = "ssl=true&replicaSet=atlas-tso1zz-shard-0&authSource=admin&retryWrites=true&w=majority"
+        db = self.credentials['db']
+        connection_string = f"mongodb://{login}@{url}/{db}?{params}"
+        return MongoClient(connection_string)
+
+    def __get_database(self):
+        return self.client[self.credentials['db']]
+
+    def get_collection(self):
+        return self.db[self.credentials['collection']]
--- planet.py
+++ planet.py
@@ -2,15 +2,10 @@ from pydantic import BaseModel
 from typing import Optional
 import requests
 import json
-from pymongo import MongoClient
+from mongo import Mongo
 
-with open('./config/credentials.json') as json_file:
-    credentials = json.load(json_file)
-client = MongoClient(
-    f"mongodb://{credentials['mongodb']['user']}:{credentials['mongodb']['pass']}@star-wars-shard-00-00.eljo1.mongodb.net:27017,star-wars-shard-00-01.eljo1.mongodb.net:27017,star-wars-shard-00-02.eljo1.mongodb.net:27017/{credentials['mongodb']['db']}?ssl=true&replicaSet=atlas-tso1zz-shard-0&authSource=admin&retryWrites=true&w=majority"
-)
-db = client['star-wars-py']
-planets = db['planets']
+mongo = Mongo()
+planets = mongo.get_collection()
 
 
 class Planet(BaseModel):
@@ -21,13 +16,10 @@ class Planet(BaseModel):
     films_no: Optional[int]
 
     def get_films_no(self):
-        results = json.loads(
-            requests.get(f"https://swapi.dev/api/planets/?search={self.name}").
-            content)['results']
-        if len(results) > 0:
-            return len(results[0]['films'])
-        else:
-            return 0
+        content = requests.get(
+            f"https://swapi.dev/api/planets/?search={self.name}").content
+        results = json.loads(content)['results']
+        return len(results[0]['films']) if len(results) > 0 else 0
 
     def insert(self):
         planet = {
@@ -41,19 +33,19 @@ class Planet(BaseModel):
 
     @staticmethod
     def get(id, name):
+        query = {}
         if id is not None:
-            p = {"_id": id}
-        elif name is not None:
-            p = {"name": name}
-        else:
-            p = {}
-        return [r for r in planets.find(p)]
+            query.update({"_id": id})
+        if name is not None:
+            query.update({"name": name})
+        return [planet for planet in planets.find(query)]
 
     @staticmethod
     def get_id():
-        return len([r for r in planets.find({})]) + 1
+        return len([planet for planet in planets.find({})]) + 1
 
     @staticmethod
     def delete(id):
-        planets.delete_one({"_id": id})
+        query = {"_id": id}
+        planets.delete_one(query)
         return {"status": 200}
